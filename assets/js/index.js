ad4d3260f7c850b808d9a114cda947f3c67b3897
Hamidou Diallo <Dio.hamidou@gmail.com>
2020-07-16 13:38:30 -0400

    update

---
+++ assets/js/index.js
@@ -0,0 +1,18 @@
+// const sideBarScroll = document.querySelector('#sidebar ul');
+// console.log(sideBarScroll);
+
+// // sideBarScroll.classList.add('sideBarHide');
+// // console.log(sideBarScroll);
+
+
+
+// sideBarScroll.addEventListener('mouseleave',ev =>{
+//     console.log('mouse entered');
+//     ev.target.classList.toggle('sideBarHide');
+
+// });
+// sideBarScroll.addEventListener('mouseenter',ev =>{
+//     console.log('mouse clicked');
+//     ev.target.classList.toggle('sideBarHide');
+
+// });
